56be3b8b08d123bfba50b8ad08449f29389daa7a
GABRIELA RODRIGUEZ <gabriela_rodriguez64@hotmail.com>
2025-06-29 13:02:43 -0300

    Primer commit

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ app.get('/products/', (req, res) => {
     })
 
 app.get('/products/:id', (req, res) => {
-    const product = products.find((item) => item.id == req.params.id);
-    res.json(product);
-    })
+    /*const product = products.find((item) => item.id == req.params.id);
+    res.json(product);*/
+    const{id} = req.params;
+    const product = products.find((item) => item.id == id);
+
+if(!product){
+    res.status(404).json({error:"Producto inexistente"});
+}
+res.json(product);
+});
+app.get('/products/search', (req, res) => {
+    console.log(req.params);
+    res.json(params);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
